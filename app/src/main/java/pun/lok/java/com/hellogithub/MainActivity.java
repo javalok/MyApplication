466f9466b336e9393963dea6528eb762f0528bbf
javalok <java.lok2011@gmail.com>
2017-08-14 12:21:38 +0545

    Initial commit javalok

---
+++ app/src/main/java/pun/lok/java/com/hellogithub/MainActivity.java
@@ -0,0 +1,19 @@
+package pun.lok.java.com.hellogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void getName(){
+        System.out.println("Hello Proejct");
+    }
+
+
+}
